2f3e60f5a043855574b9bd89d32cfd8ca34096fa
noble0423 <ryan.noble82@gmail.com>
2019-09-19 12:49:02 -0500

    adding PHP email functionality to the contact form within the arrow-form component on the contac page.

---
+++ public/assets/js/contact.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+    // Email Validation
+    //===========================================================================
+    
+    function isValidEmailAddress(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+    };
+
+    // Contact Form AJAX
+    //===========================================================================
+
+    $("#contact-send-button").on("click", function(event) {
+        // console.log("clicked");
+        event.preventDefault();
+
+        const userFirstName     = $("#firstname").val().trim();
+        const userLastName      = $("#lastname").val().trim();
+        const userFullName      = `${userFirstName} ${userLastName}`;
+        const userEmail         = $("#email").val().trim();
+        const userEmailSubject  = $("#subject").val().trim();
+        const userEmailMessage  = $("#message").val().trim();
+        const responseMessage   = $("#contact-us-form .email-ajax-response");
+
+        // console.log(`firstname: ${userFirstName}`);
+        // console.log(`lastname: ${userLastName}`);
+        console.log(`fullname: ${userFullName}`);
+        console.log(`email: ${userEmail}`);
+        console.log(`subject: ${userEmailSubject}`);
+        console.log(`message: ${userEmailMessage}`);
+
+        if (( userFirstName === "" || userLastName === "" || userEmail === "" || userEmailSubject === "" || userEmailMessage === "") || (!isValidEmailAddress(userEmail) )) {
+            responseMessage.fadeIn(500);
+            responseMessage.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+        }
+
+        else {
+            $.ajax({
+                type: "POST",
+                url: "/assets/php/contactForm.php",
+                dataType: "json",
+                data: {
+                    userEmail: userEmail,
+                    userFullName: userFullName,
+                    userEmailSubject: userEmailSubject,
+                    userEmailMessage: userEmailMessage,
+                },
+                beforeSend: function(result) {
+                    $("#contact-send-button").empty();
+                    $("#contact-send-button").append("<i class='fa fa-cog fa-spin'></i> Wait...");
+                },
+                success: function(result) {
+                    if(result.sendstatus === 1) {
+                        $("#contact-us-form .ajax-hidden").fadeOut(500);
+                        responseMessage.html(result.message).fadeIn(500);
+                    }
+
+                    else {
+                        $("#contact-us-form button").empty();
+                        $("#contact-us-form button").append("<i class='fa fa-retweet></i> Please Try Again.");
+                        responseMessage.html(result.message).fadeIn(1000);
+                    }
+                }
+            })
+        }
+    });
+
+});
---
+++ public/assets/php/contactForm.php
@@ -0,0 +1,64 @@
+<?php
+
+// Email Contact Form to:
+$to = "ryan.noble82@gmail.com";
+
+// userFullName
+// userEmail
+// userEmailSubject
+// userEmailMessage
+
+if (isset($_POST["userFullName"]) && isset($_POST["userEmail"]) && isset($_POST["userEmailSubject"]) && isset($_POST["userEmailMessage"])) {
+    $name       = $_POST["userFullName"];
+    
+    $from       = $_POST["userEmail"];
+
+    $message    = "
+                <html>
+                <head>
+                <title>HTML EMAIL</title>
+                </head>
+                <body>
+                <h4>The following message was sent from the Contact Form on your Portfolio Website:</h4>
+                <p>
+                ";
+    $message   .= $_POST["userEmailMessage"];
+    $message   .= "
+                </p>
+                <br>
+                <p><u>Customer Name: </u> {$name}<br>
+                *** Hitting <i>'reply'</i> will send a reply email to the following address: {$from}</p>
+                </body>
+                </html>
+                ";
+
+    $subject    = "Website Inquiry - '";
+    $subject   .= $_POST["userEmailSubject"];
+    $subject   .= "'";
+
+    $headers 	= "MIME-Version: 1.0" . "\r\n";
+    $headers   .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers   .= "From: {$name} <webmaster@example.com>" . "\r\n";
+    $headers   .= "Reply-To: {$from}" . "\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        $result = array(
+            "message" => "Thanks for contacting us! We will reach out to you shortly!",
+            "sendstatus" => 1
+        );
+
+        echo json_encode($result);
+    }
+
+    else {
+        $result = array(
+            "message" => "Oops, something went wrong. Please call us at (713) 252-6508.",
+            "sendstatus" => 1
+        );
+
+        echo json_encode($result);
+    }
+}
+
+
+?>
