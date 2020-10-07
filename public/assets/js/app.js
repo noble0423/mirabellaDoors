c0f7031ab8921da7adc1e67f5c189144f9ed7c44
noble0423 <ryan.noble82@gmail.com>
2020-10-07 14:39:42 -0500

    added select form field to Contact pg, added jQuery to grab that field on change and modify the font color so it matches the rest of the form.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -68,41 +68,6 @@ $(document).ready(function() {
         maxWidth: 450,
         maxHeight: 700,
     });
-    
-
-    // Button Clicks for Page Routing
-    //=====================================================================
-
-    // Mirabella Difference Btn
-    $(".mirabella-defference-btn").on("click", function() {
-        location.href = "/mirabelladifference";
-    });
-
-    // Contemporary Doors Btn
-    $(".contemporary-doors-btn").on("click", function() {
-        location.href = "/contemporarydoors";
-    });
-
-    // Traditional Doors Btn
-    $(".traditional-doors-btn").on("click", function() {
-        location.href = "/traditionaldoors";
-    });
-
-    // Door Design Library Btn
-    $(".design-library-btn").on("click", function() {
-        location.href = "/doordesignlibrary";
-    });
-
-    // Finishes Btn
-    $(".finishes-btn").on("click", function() {
-        location.href = "/doorfinishes";
-    });
-
-    // Contact Us Btn
-    $(".contact-us-btn").on("click", function() {
-        location.href = "/contactus";
-    });
-
 
     // Button Clicks for Smooth Scroll Effect
     //=====================================================================
@@ -139,5 +104,11 @@ $(document).ready(function() {
         });
     });
 
+    // Contact Us Form (selected color change)
+    $("#howDidHear").change(function(){
+        console.log("changed");
+        $("#howDidHear").css("color", "#495057");
+    })
+  
 });
 
